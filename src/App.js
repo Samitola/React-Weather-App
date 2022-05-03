7173a8428b82a9ec390d034cf21b863a7d2cfcd8
Samitola <samitola986@gmail.com>
2022-05-03 12:32:16 -0400

    cleaned up code.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Samitola/portfolio-project" target="_blank">
+          Sami Mogaji
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Samitola/react-weather-app" target="_blank">
+          open sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
