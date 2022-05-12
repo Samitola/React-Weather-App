83f7dc6be092ebe01bb0b7324f42642195bc5d40
Samitola <samitola986@gmail.com>
2022-05-12 11:05:54 -0400

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Samitola/portfolio-project" target="_blank">
-          Sami Mogaji
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/Samitola/react-weather-app" target="_blank">
-          open sourced on github
-        </a>
-      </footer>
+    <div className="App ">
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Samitola/portfolio-project"
+            target="_blank"
+          >
+            Sami Mogaji
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Samitola/react-weather-app"
+            target="_blank"
+          >
+            open sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
