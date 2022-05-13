d36e65678d36649047ba412e9a3e5eedacf04230
Samitola <samitola986@gmail.com>
2022-05-13 14:04:05 -0400

    added initial css

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,16 @@ export default function Weather() {
               type="search"
               placeholder="Type a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary"
+              w-100
+            />
           </div>
         </div>
       </form>
@@ -23,13 +29,18 @@ export default function Weather() {
         <li>Thursday 10:00am</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Cloudy"
-          />
-          65ºC
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="Cloudy"
+              className="float-left"
+            />
+
+            <span className="temperature">6</span>
+            <span className="unit">ºC</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
