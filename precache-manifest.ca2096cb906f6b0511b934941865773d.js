56e0c76d9f57236e9f26e6f8c882277bc3781974
Samitola <samitola986@gmail.com>
2022-05-17 12:22:23 -0400

    Updates

---
+++ precache-manifest.ca2096cb906f6b0511b934941865773d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8fd2ed086939c9cdfa3",
+    "url": "/my-app/static/css/main.71a5409a.chunk.css"
+  },
+  {
+    "revision": "c8fd2ed086939c9cdfa3",
+    "url": "/my-app/static/js/main.c8fd2ed0.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/my-app/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "f338acae87b4e12a2d22",
+    "url": "/my-app/static/css/2.7d5ce47f.chunk.css"
+  },
+  {
+    "revision": "f338acae87b4e12a2d22",
+    "url": "/my-app/static/js/2.f338acae.chunk.js"
+  },
+  {
+    "revision": "1ed7c516e0401e53272b",
+    "url": "/my-app/static/js/runtime~main.1ed7c516.js"
+  },
+  {
+    "revision": "c86209c5141f7040ceb57b5113ee19bf",
+    "url": "/my-app/index.html"
+  }
+];
